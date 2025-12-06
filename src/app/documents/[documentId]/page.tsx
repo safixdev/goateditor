39dcacee0c5343c7c6d20894f31a6b3f776fd8a3
safi <asafn@jfrog.com>
2025-12-06 16:51:51 +0200

    Remove navbar from document editor page

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,4 @@
 import { Editor } from "./editor";
-import { Navbar } from "./navbar";
 import { ToolBar } from "./toolbar";
 
 interface DocumentIdPageProps {
@@ -11,10 +10,9 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   return (
     <div className="min-h-screen bg-[#FAFBFD] ">
       <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
-        <Navbar />
         <ToolBar />
       </div>
-      <div className="pt-[114px] print:pt-0">
+      <div className="pt-[60px] print:pt-0">
         <Editor />
       </div>
     </div>
